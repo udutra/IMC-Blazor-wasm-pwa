23dfa5b2cdb9f280e659bd08426fd279a9873cc1
Guilherme Galarça Dutra <udutra@gmail.com>
2024-04-12 23:15:55 -0300

    Finalizado PWA

---
+++ AppState.cs
@@ -0,0 +1,14 @@
+﻿using IMC.Models;
+
+namespace IMC;
+
+public static class AppState
+{
+    public static List<ImcModel> History { get; set; } = [];
+    public static event EventHandler<EventArgs> OnChanged;
+
+    public static void NotifyChanges(object? sender, EventArgs? args)
+    {
+        OnChanged.Invoke(sender, args);
+    }
+}
---
+++ Models/ImcModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMC.Models;
+
+public class ImcModel
+{
+    [Required(ErrorMessage = "Informe a altura")]
+    [Range(1.0, 3.0, ErrorMessage = "Altura inválida")]
+    public double? Height { get; set; }
+    
+    [Required(ErrorMessage = "Informe o peso")]
+    [Range(15.0, 180.0, ErrorMessage = "Peso inválido")]
+    public double? Weight { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public double Imc => (Weight ?? 1) / ((Height ?? 1) * (Height ?? 1));
+}
